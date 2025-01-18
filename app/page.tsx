75616eea837b87918927ec16530be4a46fc8207b
marumo333 <seito5026junior@icloud.com>
2025-01-18 21:16:18 +0900

    画像添付機能付与

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import ImageApp from "@/components/imageApp";
 
-export default async function Home() {
-  return (
-    <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
-    </>
-  );
+export default function Private(){
+  <>
+  <h1 className="mb-4 pt-28 text-4xl">オシャレ服投稿アプリ</h1>
+  <p className="mb-4">
+    *表示された画像は5分でリンクが不可になります。<br/>
+    再度アクサスした場合はリロードボタンを押してください。
+  </p>
+  <div className="flex-1 w-full flex flex-col items-center">
+    <ImageApp/>
+  </div>
+  </>
 }
