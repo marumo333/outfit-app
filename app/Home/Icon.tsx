32a339d213d4340c55366c6d4a83abc2bb726581
marumo333 <seito5026junior@icloud.com>
2025-02-10 23:07:04 +0900

    fixed Icon

--- app/Home/Icon.tsx
+++ app/Home/Icon.tsx
@@ -1,29 +1,28 @@
 "use client"
 import Image from "next/image";
-import React,{FC} from "react";
+import React,{ FC } from "react";
 
 type Props = {
   url: string;
 };
 
 const Icon: FC<Props> = ({ url }) => {
-  
   return (
-    <div>
+    <div style={{ width: 200, height: 200, position: "relative", borderRadius: "50%", overflow: "hidden" }}>
       {url ? (
         <Image
           src={url}
           alt="Avatar"
-          width={200}
-          height={200}
-          style={{ borderRadius: "50%" }}
+          fill
+          style={{ objectFit: "cover" }} // 画像を適切に調整
           unoptimized={true} 
         />
       ) : (
-        <div style={{ width: 200, height: 200, borderRadius: "50%", background: "gray" }} />
+        <div style={{ width: "100%", height: "100%", background: "gray" }} />
       )}
     </div>
   );
 };
 
 export default Icon;
+
