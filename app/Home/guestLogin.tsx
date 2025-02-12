ce97561410625d12b01be868622c565020fc53db
marumo333 <seito5026junior@icloud.com>
2025-02-12 22:29:57 +0900

    add guest file change css

--- app/Home/guestLogin.tsx
+++ app/Home/guestLogin.tsx
@@ -11,7 +11,7 @@ export default function(){
 
     return(
         <>
-        <button className="bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-4 rounded-lg" onClick={guestSignIn}>
+        <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-medium py-2 px-4 rounded-lg" onClick={guestSignIn}>
             ゲストログイン
             </button>
         </>
