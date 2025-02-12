afe6409341b79d6b0ef5ca29f46c07c5105fd19b
marumo333 <seito5026junior@icloud.com>
2025-02-12 22:28:14 +0900

    add guestLogin function

--- app/Home/guestLogin.tsx
+++ app/Home/guestLogin.tsx
@@ -1,10 +1,19 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
-import React from "react";
+import { useNavigate } from 'react-router-dom';
 
-export default function GuestLogin(){
-return(
-    <>
-    </>
-)
+
+export default function(){
+    const navigate =useNavigate();
+    const guestSignIn=()=>{
+        navigate("/private")
+    }
+
+    return(
+        <>
+        <button className="bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-4 rounded-lg" onClick={guestSignIn}>
+            ゲストログイン
+            </button>
+        </>
+    )
 }
