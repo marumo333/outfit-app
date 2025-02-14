aa72e777a5f475eba228feff6537fa7a74ddd03a
marumo333 <seito5026junior@icloud.com>
2025-02-14 22:27:42 +0900

    add Home

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React, { useState, useEffect } from "react";
+
+export default function App() {
+  return (
+    <h1 
+    className="px-12 py-4 text-white text-2xl font-bold skew-x-[-15deg] bg-gradient-to-r from-[#209cff] to-[#68e0cf]"
+    >
+      服投稿アプリ
+    </h1>
+  );
+}
