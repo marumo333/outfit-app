da6e2a5eae4817d3c2ef0e46fd60ed2a29ab1ffa
marumo333 <seito5026junior@icloud.com>
2025-02-14 16:04:16 +0900

    add serch

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React,{useState,useEffect} from "react";
 import { supabase } from "@/utils/supabase/supabase";
+import Head from "next/head";
 
 export default function Serach(){
     const [posts,setPosts] = useState([]);
@@ -15,11 +16,27 @@ export default function Serach(){
         .from("outfit-app")
         .select("*");
         setPosts(data);
+    };
+
+    const search = async(value:string) =>{
+        if(value!==""){
+            const {data:post,error} = await supabase
+            .from("outfit-app")
+            .select()
+            .textSearch("title", `${value}`);
+            if(error) throw new Error ;
+            setPosts(posts)
+            return;
+        }else{
+            await fetchPosts();
+        }
+        };
+
+        return(
+            <>
+            </>
+        )
     }
 
+
     
-    return(
-        <>
-        </>
-    )
-}
