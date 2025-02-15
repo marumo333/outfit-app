79302fd4e800c012f4c5e1d2e200d6f876bc2f6b
marumo333 <seito5026junior@icloud.com>
2025-02-15 23:47:02 +0900

    fixed 500 error

--- app/clientWrapper.tsx
+++ app/clientWrapper.tsx
@@ -2,12 +2,11 @@
 import { Provider } from "react-redux";
 import store from "./store";
 import React from "react";
-import { BrowserRouter } from "react-router-dom"
+
 
 export default function ClientWrapper({ children }: { children: React.ReactNode }) {
   return <Provider store={store}>
-    <BrowserRouter>
       {children}
-    </BrowserRouter></Provider>; // ← `Provider` が使われていることを確認
+   </Provider>; // ← `Provider` が使われていることを確認
 }
 
