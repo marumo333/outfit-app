6832ad9a4d865a3d18872beffae64ede40535f44
marumo333 <seito5026junior@icloud.com>
2025-02-15 23:26:06 +0900

    add save token

---
+++ app/redirect/page.tsx
@@ -0,0 +1,49 @@
+"use client"
+import { useSelector,useDispatch } from "react-redux";
+import React,{useEffect,useState} from "react";
+import { useCookies } from "react-cookie";
+import { supabase} from "@/utils/supabase/supabase";
+import { signOut,signIn } from "../authSlice";
+
+
+export default function Redirect(){
+    const auth = useSelector((state: any) => state.auth.isSignIn);
+  const dispatch = useDispatch()
+  const [user, setUser] = useState("")//ログイン情報を保持するステート
+  const [cookies] = useCookies()
+    const [avatarUrl, setAvatarUrl] = useState<string>(""); // URLを保存する状態
+    
+
+    useEffect(() => {
+      const { data: authListener } = supabase.auth.onAuthStateChange(
+        (event, session) => {
+          console.log(event)
+          if (session?.user) {
+            setUser(session.user.email || "GitHub User")
+            dispatch(signIn({
+              name: session.user.email,
+              iconUrl: "",
+              token: session.provider_token
+            }))
+            window.localStorage.setItem('oauth_provider_token', session.provider_token || "");
+            window.localStorage.setItem('oauth_provider_refresh_token', session.provider_refresh_token || "")
+          }
+  
+          if (event === 'SIGNED_OUT') {
+            window.localStorage.removeItem('oauth_provider_token')
+            window.localStorage.removeItem('oauth_provider_refresh_token')
+            setUser("")//user情報をリセット
+            dispatch(signOut());
+          }
+        }
+      );
+      //クリーンアップ処理追加（リスナー削除）
+      return () => {
+        authListener?.subscription.unsubscribe();
+      };
+    }, [dispatch]);
+    return(
+        <>
+        </>
+    )
+}
