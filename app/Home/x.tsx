67a06983da0fe6f600576929ae1434c585552db3
marumo333 <seito5026junior@icloud.com>
2025-02-18 12:26:39 +0900

    add x login  button

--- app/Home/x.tsx
+++ app/Home/x.tsx
@@ -50,8 +50,57 @@ export default function X(){
           router.push("/private")
         }
       }, [user, router])
+      const signInX = async () => {
+          const { error } = await supabase.auth.signInWithOAuth({
+            provider: 'google',
+            options: {
+              redirectTo: `http://localhost:3000/redirect`,
+            },
+          })
+          router.push("/private")
+          if (error) throw new Error(error.message)
+        }
+      
+        const signOutX = async () => {
+          try {
+            const { error } = await supabase.auth.signOut()
+            if (error) throw new Error(error.message)
+            dispatch(signOut());
+          }
+          catch (error: any) {
+            console.error("ログアウトエラー発生", error.message)
+          }
+        }
+      
+        useEffect(() => {
+          if (!user) return; // user が null の場合は何もしない
+      
+          const fetchAvatarUrl = async () => {
+            const { data } = supabase.storage.from("avatars").getPublicUrl("x.jpg");
+            setAvatarUrl(data.publicUrl || "");
+          };
+      
+          fetchAvatarUrl();
+        }, [user]);
     return(
         <>
+        <div className="flex flex-col items-center justify-center space-y-4 min-h-screen bg-gray-100">
+      <button onClick={signInX} className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-lg">Xでログイン</button>
+      {user ? (
+        <button
+          onClick={signOutX}
+          className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-lg">
+          Xでログアウト
+        </button>
+      ) : (
+        <div className="text-gray-500" color="blue"><p>ログイン情報を取得中:</p></div>
+      )
+      }
+      {user ? (
+        <Icon url={avatarUrl} />) : (
+        <div className="text-gray-500" color="green"><span>アイコンを取得してください</span></div>
+      )}
+    </div>
         </>
     )
 }
