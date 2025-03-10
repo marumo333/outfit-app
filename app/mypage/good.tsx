0e1ce70fe94c54d9a90b5a1b57c3644a4c6847b2
marumo333 <seito5026junior@icloud.com>
2025-03-10 20:04:33 +0900

    add heroicons

--- app/mypage/good.tsx
+++ app/mypage/good.tsx
@@ -1,7 +1,9 @@
 "use client"
 import React,{useState,useEffect} from "react";
+import { HeartIcon } from '@heroicons/react/24/solid'
 
 export default function Good(){
+    const [goods,setGoods]= useState("")
     return(
         <>
         </>
