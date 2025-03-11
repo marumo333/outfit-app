a4ea20df0cc44e95271d03708cb2424cdbdb442f
marumo333 <seito5026junior@icloud.com>
2025-03-11 19:26:44 +0900

    add getUser function

--- app/image/[id]/goodSection.tsx
+++ app/image/[id]/goodSection.tsx
@@ -10,7 +10,38 @@ interface GoodThread{
 }
 export const GoodSection=()=>{
     const [good,setGood] = useState<GoodThread[]>([])
+    const [user,setUser]=useState<string>('')
 
+
+    useEffect(() => {
+            const fetchUser =async()=>{
+                const { data,error} = await supabase.auth.getUser();
+                console.log(user)
+                if(error){
+                    console.log("ユーザー情報を取得だきませんでした",error);
+                    return;
+                }
+                if(data?.user){
+                    setUser(data.user.id);
+              }
+            }
+            fetchUser();
+        }, []);
+
+    const handleLikeSubmit=async (event: React.FormEvent<HTMLFormElement>)=>{
+        event.preventDefault();
+        if (user!) {
+            // いいねを新規作成
+            await supabase.from('likes').insert({
+              user_id: user.id!,
+              comment_id,
+            })
+          } else {
+            // いいねを削除
+            await supabase.from('likes').delete().match({ user_id: user.id, comment_id: comment_id })
+          }
+
+    }
     return(
         <>
         </>
