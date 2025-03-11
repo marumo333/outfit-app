99178c38a871825c2ad552d208500e05834c54c9
marumo333 <seito5026junior@icloud.com>
2025-03-11 15:33:39 +0900

    add interfact goodthread

--- app/image/[id]/goodSection.tsx
+++ app/image/[id]/goodSection.tsx
@@ -1,13 +1,19 @@
 "use client"
 import { HeartIcon } from '@heroicons/react/24/solid'
-
+import { supabase } from "@/utils/supabase/supabase";
+import React,{useState,useEffect} from "react";
+import router from 'next/router';
 interface GoodThread{
-    
+    id:number,
+    user_id:string,
+    comment_id:string,
 }
 export const GoodSection=()=>{
+    const [good,setGood] = useState<GoodThread[]>([])
+
     return(
-    <>
-    </>
+        <>
+        </>
     )
 }
 export default GoodSection;
