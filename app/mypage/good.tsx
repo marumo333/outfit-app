947791dae47fed65091d03fa76e32df594fc7e35
marumo333 <seito5026junior@icloud.com>
2025-03-12 22:15:58 +0900

    fix fetchLikes add data

--- app/mypage/good.tsx
+++ app/mypage/good.tsx
@@ -1,45 +1,40 @@
 "use client"
 import { supabase } from "@/utils/supabase/supabase";
 import { eq } from "lodash";
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 
-export default function Good(){
-    const [imageId,setImageId]= useState<string>("")
+export default function Good() {
+    const [imageId, setImageId] = useState<string>("")
     const [userId, setUserId] = useState<string>("")
-    
-        useEffect(() => {
-            const fetchLikes = async () => {
-                if (!imageId || !userId) return;
-                const { data, error } = await supabase
-                    .from('likes')
-                    .select("*", { count: "exact" })
-                    .eq("image_id", imageId);
-                    .eq("user_id",userId);
-                    .maybeSingle()
-    
-                if (!error) {
-                    console.error('お気に入りページが取得できませんでした',error)
-                }
-                const { data: likeData } = await supabase
-                    .from('likes')
-                    .select("*", { count: "exact" })
-                    .eq("image_id", imageId)
-                    .eq("user_id", userId)
-                    .maybeSingle()
-    
-                if (likeData) {
-                    setUserId(likeData.user_id);
-                    setImageId(likeData.image_id);
-                }
-            };
-            fetchLikes();
-        }, [userId, imageId])
-    return(
+
+    useEffect(() => {
+        const fetchLikes = async () => {
+            if (!imageId || !userId) return;
+            const { data, error } = await supabase
+                .from('likes')
+                .select("*", { count: "exact" })
+                .eq("image_id", imageId)
+                .eq("user_id", userId)
+                .maybeSingle()
+
+            if (error) {
+                console.error('いいねの投稿を取得だきませんでした', error.message);
+                return;
+            }
+
+            if (!data) return;
+            setUserId(data.user_id);
+            setImageId(data.image_id);
+
+        };
+        fetchLikes();
+    }, [userId, imageId])
+    return (
         <>
-        <h1>お気に入りの投稿</h1>
-        <p>{userId}</p>
-        <p>{imageId}</p>
+            <h1>お気に入りの投稿</h1>
+            <p>ユーザーID: {userId || "未設定"}</p>
+            <p>画像ID: {imageId || "未設定"}</p>
         </>
     )
 }
