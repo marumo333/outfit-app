03335086d8a1ffddd72823a667256d2d903ac23f
marumo333 <seito5026junior@icloud.com>
2025-03-28 20:40:09 +0900

    header部分にcssが重ならないように修正

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -160,7 +160,11 @@ export default function Mypage() {
             {userId ? (
                 <>
                     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-                        <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]" aria-hidden="true">
+                        <div
+                            className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem] pointer-events-none"
+                            aria-hidden="true"
+                        >
+
                             <div
                                 className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
                                 style={{
